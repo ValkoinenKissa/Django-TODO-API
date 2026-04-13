f07221a47212a38580f1cf4f34f16c2a3692ccd5
Alberto Bollo <albertobollo7@protonmail.com>
2026-04-13 12:48:49 +0200

    Added logic to dynamically list the tasks in the array

--- misitio/tareas/views.py
+++ misitio/tareas/views.py
@@ -1,7 +1,19 @@
+import tareas
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("Bienvenido a la aplicación de tareas")
+    tareas = [
+        "Estudiar python",
+        "Hacer ejercicio",
+        "Leer un libro"
+    ]
+
+    texto = "<h1>Lista de tareas</h1>"
+
+    for tarea in tareas:
+        texto += f"<p>{tarea}</p>"
+
+    return HttpResponse(texto)
